e52eb56a756332b6c057c98a54323d1009d5f15b
Tim Knops <timknops22@gmail.com>
2022-09-25 23:05:13 +0200

    Fixing the loop in 3c

--- oefenopdracht3c-strings-vervolg/src/StringsVervolg.java
+++ oefenopdracht3c-strings-vervolg/src/StringsVervolg.java
@@ -29,34 +29,42 @@ public class StringsVervolg {
         }
         for (int i = 0; i < secondSentenceWords.length; i++) {
             System.out.printf("De eerste letter van woord %d: %c\n", i, secondSentenceWords[i].charAt(0));
-        }
+        } // Prints out the input string in uppercase
         System.out.printf("De zin in hoofdletters: %s\n\n", secondSentence.toUpperCase());
 
         System.out.print("Geef een derde zin van drie woorden: ");
         String thirdSentence = input.nextLine();
 
+        // Checks to see if the first string input is equal to the third string input
         if (firstSentence.equals(thirdSentence)) {
             System.out.println("Zin 3 is gelijk aan zin 1");
         } else {
             System.out.println("Zin 3 is niet gelijk aan zin 1");
         }
 
+        // Checks the same thing as the previous if-else block, however it will overlook casing.
         if (firstSentence.equalsIgnoreCase(thirdSentence)) {
             System.out.println("Zin 3 is gelijk aan zin 1 als we hoofdletters negeren");
         } else {
             System.out.println("Zin 3 is ook niet gelijk aan zin 1 als we hoofdletters negeren");
         }
 
+        // Splits the third sentence into words and stores it in an array,
+        // then uses a nested for loop to iterate through both arrays to check if words match
         String[] thirdSentenceWords = thirdSentence.split(" ");
         for (String secondWord : secondSentenceWords) {
+            boolean value = false;
             for (String thirdWord : thirdSentenceWords) {
                 if (secondWord.equals(thirdWord)) {
-                    System.out.printf("Het woord \"%s\" komt wel voor in zin 3\n", secondWord);
-                    break;
-                } else {
-                    System.out.printf("Het woord \"%s\" komt niet voor in zin 3\n", secondWord);
+                    value = true;
                     break;
                 }
+            }
+
+            if (value) {
+                System.out.printf("Het woord \"%s\" komt wel voor in zin 3\n", secondWord);
+            } else {
+                System.out.printf("Het woord \"%s\" komt niet voor in zin 3\n", secondWord);
             }
         }
     }
